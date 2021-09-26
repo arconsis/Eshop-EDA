5bce6df06d2cdbbad0f82bd2a431345778e13c78
Dimosthenis Botsaris <botsaris.d@gmail.com>
2021-09-26 09:58:36 +0300

    interaction among order and shipment

--- v1/packages/EmailsService/src/common/constants.js
+++ v1/packages/EmailsService/src/common/constants.js
@@ -1,16 +1,13 @@
 const SENDER_REGISTRATION_EMAIL = 'dimosthenis.botsaris@arconsis.com';
-
 const REGISTRATION_EMAIL_SUBJECT = 'Welcome onboard!';
-
 const ORDERS_TOPIC = 'Orders';
-
 const ORDER_CONFIRMED_SUBJECT = 'New order confirmed';
-
 const ORDER_CONFIRMED_EVENT_TYPE = 'OrderConfirmed';
-
 const ORDER_CREATED_EVENT_TYPE = 'OrderCreated';
-
 const ORDER_CREATED_SUBJECT = 'New order was placed!';
+const SHIPMENTS_TOPIC = 'Shipments';
+const SHIPMENT_PREPARED_EVENT_TYPE = 'ShipmentPrepared';
+const SHIPMENT_PREPARED_CONFIRMED_SUBJECT = 'The order is our for shipment';
 
 
 module.exports = {
@@ -21,4 +18,7 @@ module.exports = {
   ORDER_CONFIRMED_SUBJECT,
   ORDER_CREATED_EVENT_TYPE,
   ORDER_CREATED_SUBJECT,
+  SHIPMENTS_TOPIC,
+  SHIPMENT_PREPARED_EVENT_TYPE,
+  SHIPMENT_PREPARED_CONFIRMED_SUBJECT,
 };
