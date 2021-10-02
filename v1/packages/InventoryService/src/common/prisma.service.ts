557d126f180ec8abf361f018df91d0073d6cad38
Alex Koufatzis <akoufa@hotmail.com>
2021-10-02 09:27:49 +0200

    Prisma should wait for app close before exiting

--- v1/packages/InventoryService/src/common/prisma.service.ts
+++ v1/packages/InventoryService/src/common/prisma.service.ts
@@ -7,7 +7,7 @@ export class PrismaService extends PrismaClient implements OnModuleInit {
     await this.$connect();
   }
 
-  async enableShutdownHooks(app: INestApplication) {
+  enableShutdownHooks(app: INestApplication) {
     this.$on('beforeExit', async () => {
       await app.close();
     });
--- v1/packages/InventoryService/src/main.ts
+++ v1/packages/InventoryService/src/main.ts
@@ -1,11 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './common/prisma.service';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableShutdownHooks();
 
+  const prismaService: PrismaService = app.get(PrismaService);
+  prismaService.enableShutdownHooks(app);
+
   await app.listen(3000);
 }
+
 bootstrap();
