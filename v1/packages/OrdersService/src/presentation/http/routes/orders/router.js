9a8a1e950729093c50feee215aa9a236eb7c2bf3
Dimosthenis Botsaris <botsaris.d@gmail.com>
2021-10-04 13:54:32 +0300

    fix #24 --- validate orders payload

--- v1/packages/OrdersService/src/presentation/http/routes/orders/router.js
+++ v1/packages/OrdersService/src/presentation/http/routes/orders/router.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const asyncWrapper = require('../../utils/asyncWrapper');
+const {
+  requestOrderRules,
+  validate,
+} = require('../../middleware/routerValidator');
 
 const router = express.Router({ mergeParams: true });
 
 function init({ ordersService }) {
   router.post('/',
-    // (...args) => endpointValidator.checkParamsToRefreshToken(...args),
+    requestOrderRules(),
+    validate,
     asyncWrapper(async (req, res) => {
       const result = await ordersService.requestOrder({
         userId: req.body.userId,
@@ -19,7 +24,6 @@ function init({ ordersService }) {
       });
     }));
   router.get('/:orderNo',
-    // (...args) => endpointValidator.checkParamsToRefreshToken(...args),
     asyncWrapper(async (req, res) => {
       const result = await ordersService.getOrder(req.params.orderNo);
       return res.status(200).send({
