f535f8e71b87c2fe85965162f07646de809473b9
Dimosthenis Botsaris <botsaris.d@gmail.com>
2021-10-28 11:28:08 +0300

    EDA-18: improve db module

--- terraform/modules/database/variables.tf
+++ terraform/modules/database/variables.tf
@@ -2,6 +2,13 @@ variable "database_identifier" {
   description = "Database identifier"
 }
 
+variable "database_name" {
+  description = "Database name"
+  type        = string
+  sensitive   = true
+  default     = "postgres"
+}
+
 variable "database_username" {
   description = "The password for the DB master"
   type        = string
@@ -33,4 +40,64 @@ variable "security_group_ids" {
 variable "monitoring_role_name" {
   description = "Monitoring role name of the DB"
   type        = string
+}
+
+variable "database_engine" {
+  description = "DB engine"
+  type        = string
+  default     = "postgres"
+}
+
+variable "database_engine_version" {
+  description = "DB engine version"
+  type        = string
+  default     = "11.12"
+}
+
+variable "database_auto_minor_version_upgrade" {
+  description = "DB auto minor version upgrade"
+  type        = bool
+  default     = false
+}
+
+variable "database_family" {
+  description = "DB family"
+  type        = string
+  default     = "postgres11"
+}
+
+variable "database_engine_major_engine_version" {
+  description = "DB engine major version"
+  type        = string
+  default     = "11"
+}
+
+variable "database_instance_class" {
+  description = "DB instance class"
+  type        = string
+  default     = "db.t3.small"
+}
+
+variable "database_maintenance_window" {
+  description = "DB maintenance window"
+  type        = string
+  default     = "Mon:00:00-Mon:03:00"
+}
+
+variable "database_backup_window" {
+  description = "DB backup window"
+  type        = string
+  default     = "03:00-06:00"
+}
+
+variable "database_enabled_cloudwatch_logs_exports" {
+  description = "DB enabled cloudwatch logs exports"
+  type        = list(string)
+  default     = ["postgresql", "upgrade"]
+}
+
+variable "database_parameters" {
+  description = "DB parameters"
+  type        = list(map(string))
+  default     = []
 }
