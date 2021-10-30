bfa2ac4adea4e00db54859e288ee244b6d63d1f6
Alex Koufatzis <akoufa@hotmail.com>
2021-10-30 21:58:45 +0200

    EDA-12: Remove unused code and improve code structure. Add commit transaction and close procuder

--- v3/services/OrderService/src/main/kotlin/com/arconsis/common/Constants.kt
+++
@@ -1,5 +0,0 @@
-package com.arconsis.common
-
-enum class Topics {
-  Orders
-}
--- v3/services/OrderService/src/main/kotlin/com/arconsis/domain/orders/OrdersService.kt
+++ v3/services/OrderService/src/main/kotlin/com/arconsis/domain/orders/OrdersService.kt
@@ -9,37 +9,46 @@ import org.apache.kafka.common.KafkaException
 import java.util.*
 import javax.enterprise.context.ApplicationScoped
 
-
 @ApplicationScoped
 class OrdersService {
-  fun createOrder(orderCreateDto: OrderCreateDto): Order {
-    val orderNo = UUID.randomUUID()
-    val pendingOrder = orderCreateDto.toPendingOrder(orderNo)
-    val event = createRequestOrderEvent(
-      pendingOrder,
-    )
-    createProducer().use { kafkaProducer ->
-      kafkaProducer.initTransactions()
-      try {
-        kafkaProducer.beginTransaction()
-        kafkaProducer.send(ProducerRecord(Topics.Orders.name, event.first, event.second))
-      } catch(e: KafkaException)  {
-        kafkaProducer.abortTransaction()
-      }
-    }
-    return pendingOrder
-  }
 
-  private fun createProducer(): KafkaProducer<String, Order> {
-    val producerConfig = Properties()
-    producerConfig[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = "localhost:9092"
-    producerConfig[ProducerConfig.TRANSACTIONAL_ID_CONFIG] = "OrderServiceInstance1"
-    producerConfig[ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG] = "true"
-    producerConfig[ProducerConfig.RETRIES_CONFIG] = Int.MAX_VALUE.toString()
-    producerConfig[ProducerConfig.ACKS_CONFIG] = "all"
-    producerConfig[ProducerConfig.CLIENT_ID_CONFIG] = "order-service-producer"
-    producerConfig["key.serializer"] = "org.apache.kafka.common.serialization.StringSerializer"
-    producerConfig["value.serializer"] = OrderDtoSerializer::class.java
-    return KafkaProducer(producerConfig)
-  }
+    fun createOrder(orderCreateDto: OrderCreateDto): Order {
+
+        val orderNo = UUID.randomUUID()
+        val pendingOrder = orderCreateDto.toPendingOrder(orderNo)
+        val event = creatOrderRequestEventPair(
+            pendingOrder,
+        )
+
+        sendOrderEvent(event)
+        return pendingOrder
+    }
+
+    private fun sendOrderEvent(eventPair: Pair<String, Order>) {
+        val kafkaProducer = createProducer()
+        kafkaProducer.initTransactions()
+        try {
+            kafkaProducer.beginTransaction()
+            // Blocking until https://issues.apache.org/jira/browse/KAFKA-12227
+            kafkaProducer.send(ProducerRecord(Topics.ORDERS.topicName, eventPair.first, eventPair.second)).get()
+            kafkaProducer.commitTransaction()
+            kafkaProducer.close()
+        } catch (e: KafkaException) {
+            kafkaProducer.abortTransaction()
+        }
+    }
+
+    private fun createProducer(): KafkaProducer<String, Order> {
+        val producerConfig = Properties()
+        // TODO: Get these values from env vars
+        producerConfig[ProducerConfig.BOOTSTRAP_SERVERS_CONFIG] = "localhost:9092"
+        producerConfig[ProducerConfig.TRANSACTIONAL_ID_CONFIG] = "OrderServiceInstance1"
+        producerConfig[ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG] = "true"
+        producerConfig[ProducerConfig.RETRIES_CONFIG] = Int.MAX_VALUE.toString()
+        producerConfig[ProducerConfig.ACKS_CONFIG] = "all"
+        producerConfig[ProducerConfig.CLIENT_ID_CONFIG] = "order-service-producer"
+        producerConfig[ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG] = "org.apache.kafka.common.serialization.StringSerializer"
+        producerConfig[ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG] = OrderSerializer::class.java
+        return KafkaProducer(producerConfig)
+    }
 }
