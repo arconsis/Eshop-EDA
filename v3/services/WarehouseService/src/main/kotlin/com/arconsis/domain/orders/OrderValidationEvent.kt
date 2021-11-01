60466ad7a3ecaa9658dfe9b6ea2366a031a97cb5
Alex Koufatzis <akoufa@hotmail.com>
2021-11-01 12:17:59 +0100

    EDA-12: Use quarkus kafka streams plugin to build topology. Improve code structure

---
+++ v3/services/WarehouseService/src/main/kotlin/com/arconsis/domain/orders/OrderValidationEvent.kt
@@ -0,0 +1,30 @@
+package com.arconsis.domain.orders
+
+import java.util.*
+
+data class OrderValidationEvent(
+    val key: String,
+    val value: OrderValidationDto,
+)
+
+data class OrderValidationDto(
+    val type: String,
+    val productId: String,
+    val quantity: Int,
+    val orderNo: UUID,
+)
+
+enum class OrderValidationType(val type: String) {
+    ORDER_VALIDATED("OrderValidated"),
+    ORDER_INVALID("OrderInvalid"),
+}
+
+fun Order.toOrderValidationEvent(type: OrderValidationType) = OrderValidationEvent(
+    key = UUID.randomUUID().toString(),
+    value = OrderValidationDto(
+        type = type.type,
+        productId = productId,
+        quantity = quantity,
+        orderNo = orderNo
+    )
+)
