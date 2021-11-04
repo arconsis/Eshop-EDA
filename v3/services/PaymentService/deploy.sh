878ba9589cb70187fd8ad30055e786e0550fbe81
Dimosthenis Botsaris <botsaris.d@gmail.com>
2021-11-04 09:00:30 +0200

    EDA-12: add k8s files

---
+++ v3/services/PaymentService/deploy.sh
@@ -0,0 +1,2 @@
+docker build . -t eldimious/payment-eda-v3:latest
+docker push eldimious/payment-eda-v3:latest
---
+++ v3/services/ShippingService/deploy.sh
@@ -0,0 +1,2 @@
+docker build . -t eldimious/shipping-eda-v3:latest
+docker push eldimious/shipping-eda-v3:latest
