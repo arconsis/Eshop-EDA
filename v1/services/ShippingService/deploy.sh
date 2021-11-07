e45811ec0c543abadc665a9dcaf9d437cb47f544
Alex Koufatzis <akoufa@hotmail.com>
2021-11-07 22:36:13 +0100

    EDA-61: Remove shipping service

--- v1/services/ShippingService/deploy.sh
+++
@@ -1,2 +0,0 @@
-docker build . -t eldimious/shipping-eda-v3:latest
-docker push eldimious/shipping-eda-v3:latest
--- v1/services/ShippingService/src/main/kotlin/com/arconsis/common/Topics.kt
+++
@@ -1,6 +0,0 @@
-package com.arconsis.common
-
-enum class Topics(val topicName: String) {
-    ORDERS("Orders"),
-    SHIPMENTS("Shipments"),
-}
