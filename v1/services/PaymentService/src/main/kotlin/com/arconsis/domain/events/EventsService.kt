7e9fe5a2e1abaa6f5fbc3a6aa68f4df2f0366fba
Dimosthenis Botsaris <botsaris.d@gmail.com>
2021-11-15 12:46:25 +0200

    EDA-72: use .map and .flatmap

--- v1/services/PaymentService/src/main/kotlin/com/arconsis/domain/events/EventsService.kt
+++ v1/services/PaymentService/src/main/kotlin/com/arconsis/domain/events/EventsService.kt
@@ -9,14 +9,11 @@ import com.arconsis.domain.payments.PaymentStatus
 import com.arconsis.domain.payments.toPaymentRecord
 import io.smallrye.mutiny.Uni
 import io.smallrye.reactive.messaging.MutinyEmitter
-import io.smallrye.reactive.messaging.annotations.Blocking
 import io.smallrye.reactive.messaging.kafka.Record
-import kotlinx.coroutines.delay
 import org.eclipse.microprofile.reactive.messaging.Channel
 import org.eclipse.microprofile.reactive.messaging.Incoming
 import java.time.Duration
 import javax.enterprise.context.ApplicationScoped
-import javax.transaction.Transactional
 
 @ApplicationScoped
 class EventsService(
@@ -24,8 +21,6 @@ class EventsService(
     private val paymentsRepository: PaymentsRepository,
 ) {
     @Incoming("orders-in")
-    @Blocking
-    @Transactional
     fun consumeOrderEvents(orderRecord: Record<String, Order>): Uni<Void> {
         val value = orderRecord.value()
         return when (value.status) {
