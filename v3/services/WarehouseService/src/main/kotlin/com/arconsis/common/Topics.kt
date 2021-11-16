bbda36b6f85fb0fd581a6478b24c34709924960c
Dimosthenis Botsaris <botsaris.d@gmail.com>
2021-11-16 13:48:30 +0200

    EDA-69: merge shipment service to warehouse service

--- v3/services/WarehouseService/src/main/kotlin/com/arconsis/common/Topics.kt
+++ v3/services/WarehouseService/src/main/kotlin/com/arconsis/common/Topics.kt
@@ -3,5 +3,6 @@ package com.arconsis.common
 enum class Topics(val topicName: String) {
     ORDERS("Orders"),
     WAREHOUSE("Warehouse"),
-    ORDERS_VALIDATIONS("OrdersValidations")
+    ORDERS_VALIDATIONS("OrdersValidations"),
+    SHIPMENTS("Shipments")
 }
--- v3/services/WarehouseService/src/main/kotlin/com/arconsis/domain/events/SteamsService.kt
+++ v3/services/WarehouseService/src/main/kotlin/com/arconsis/domain/events/SteamsService.kt
@@ -1,8 +1,7 @@
 package com.arconsis.domain.events
 
 import com.arconsis.common.Topics
-import com.arconsis.domain.orders.Order
-import com.arconsis.domain.orders.isPaid
+import com.arconsis.domain.orders.*
 import com.arconsis.domain.shipments.Shipment
 import com.arconsis.domain.shipments.ShipmentStatus
 import com.arconsis.domain.shipments.toShipmentEvent
@@ -16,12 +15,40 @@ import javax.enterprise.context.ApplicationScoped
 import javax.enterprise.inject.Produces
 
 @ApplicationScoped
-class StreamsService {
+class SteamsService {
 
     @Produces
-    fun buildTopology(): Topology {
+    fun createTopology(): Topology {
         val builder = StreamsBuilder()
         val orderTopicSerde = ObjectMapperSerde(Order::class.java)
+
+        createOrdersValidationsStream(builder, orderTopicSerde)
+        createShipmentsStream(builder, orderTopicSerde)
+        return builder.build()
+    }
+
+    private fun createOrdersValidationsStream(
+        builder: StreamsBuilder,
+        orderTopicSerde: ObjectMapperSerde<Order>,
+    ) {
+        val orderValidationSerde = ObjectMapperSerde(OrderValidation::class.java)
+        builder
+            .stream(
+                Topics.ORDERS.topicName,
+                Consumed.with(Serdes.String(), orderTopicSerde)
+            )
+            .filter { _, order -> order.isPending }
+            .mapValues { order ->
+                // TODO: Add logic to check validity of the order
+                val event = order.toOrderValidationEvent(OrderValidationType.VALID)
+                event.value
+            }.to(Topics.ORDERS_VALIDATIONS.topicName, Produced.with(Serdes.String(), orderValidationSerde))
+    }
+
+    private fun createShipmentsStream(
+        builder: StreamsBuilder,
+        orderTopicSerde: ObjectMapperSerde<Order>,
+    ) {
         val shipmentTopicSerde = ObjectMapperSerde(Shipment::class.java)
         builder
             .stream(
@@ -34,7 +61,5 @@ class StreamsService {
                 val event = order.toShipmentEvent(ShipmentStatus.OUT_FOR_SHIPMENT)
                 event.value
             }.to(Topics.SHIPMENTS.topicName, Produced.with(Serdes.String(), shipmentTopicSerde))
-
-        return builder.build()
     }
 }
