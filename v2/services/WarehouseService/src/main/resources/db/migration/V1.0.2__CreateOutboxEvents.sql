815fd0338beb6c22df040f5632fed6ab771ba5b2
Dimosthenis Botsaris <botsaris.d@gmail.com>
2021-11-21 18:44:56 +0200

    EDA-100: add outbox tables

--- v2/services/WarehouseService/src/main/resources/db/migration/V1.0.2__CreateOutboxEvents.sql
+++ v2/services/WarehouseService/src/main/resources/db/migration/V1.0.2__CreateOutboxEvents.sql
@@ -6,8 +6,8 @@ CREATE TYPE aggregate_type AS ENUM (
 CREATE TABLE outbox_events
 (
     id              UUID PRIMARY KEY,
-    aggregateType   aggregate_type NOT NULL,
-    aggregateId     UUID NOT NULL UNIQUE,
+    aggregate_type  aggregate_type NOT NULL,
+    aggregate_id    UUID NOT NULL UNIQUE,
     payload         JSONB NOT NULL,
     created_at      TIMESTAMP,
     updated_at      TIMESTAMP
