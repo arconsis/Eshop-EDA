17d0902a5ad34daf1bd2e552e2fd34fe40473ecd
Alexandros Koufatzis <akoufa@hotmail.com>
2021-11-21 20:16:32 +0100

    EDA-18 Setup debezium in docker compose

---
+++ v2/init_connector.sh
@@ -0,0 +1,15 @@
+curl -H 'Content-Type: application/json' debezium:8083/connectors --data '
+{
+  "name": "orders-connector",
+  "config": {
+    "connector.class": "io.debezium.connector.postgresql.PostgresConnector",
+    "plugin.name": "pgoutput",
+    "database.hostname": "postgres",
+    "database.port": "5432",
+    "database.user": "postgres",
+    "database.password": "secret",
+    "database.dbname" : "orders-db",
+    "database.server.name": "postgres",
+    "table.include.list": "public.orders"
+  }
+}'
