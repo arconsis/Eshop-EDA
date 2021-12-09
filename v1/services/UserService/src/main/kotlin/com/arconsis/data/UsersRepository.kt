0c57c87545397eb0f5cdfe1bd0cdd9422f5a3aa3
Vagelis Goudouras <goudourasv@gmail.com>
2021-12-09 15:54:36 +0200

    EDA-98: Throw a Not found exception with when I try to fetch a user that doesn't exists, add a missing file

--- v1/services/UserService/src/main/kotlin/com/arconsis/data/UsersRepository.kt
+++ v1/services/UserService/src/main/kotlin/com/arconsis/data/UsersRepository.kt
@@ -3,9 +3,13 @@ package com.arconsis.data
 import com.arconsis.domain.User
 import com.arconsis.presentation.http.dto.CreateUser
 import io.quarkus.elytron.security.common.BcryptUtil
+import java.io.IOException
 import java.util.*
 import javax.enterprise.context.ApplicationScoped
 import javax.persistence.EntityManager
+import javax.ws.rs.ClientErrorException
+import javax.ws.rs.NotFoundException
+import javax.ws.rs.core.Response
 
 @ApplicationScoped
 class UsersRepository(private val entityManager: EntityManager) {
@@ -26,7 +30,7 @@ class UsersRepository(private val entityManager: EntityManager) {
     }
 
     fun getUser(userId: UUID): User? {
-        val userEntity = entityManager.find(UserEntity::class.java, userId) ?: return null
+        val userEntity = entityManager.find(UserEntity::class.java, userId) ?: throw ClientErrorException(Response.Status.NOT_FOUND)
         return userEntity.toUser()
     }
 }
