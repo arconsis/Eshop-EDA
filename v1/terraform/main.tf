fef2201a983809d960bfd0f6082ff2017bb2a53e
eldimious <botsaris.d@gmail.com>
2022-03-13 20:38:24 +0200

    Tf fixes

--- v1/terraform/main.tf
+++ v1/terraform/main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   shared_credentials_file = "$HOME/.aws/credentials"
-  profile                 = var.aws_profile
+  profile                 = "default"
   region                  = var.aws_region
 }
 
